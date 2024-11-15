5da8052014d065d5e473cd1505b1e8e8bb91cc6b
Tim Eder <apfelmusbanane@gmail.com>
2024-11-15 16:57:23 +0100

    feat: optimize images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,38 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import AutoImport from "astro-auto-import";
+
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://meister-eder.github.io",
-	base: "shroom-mates",
+  site: "https://meister-eder.github.io",
+  base: "shroom-mates",
+  integrations: [// Make sure the MDX integration is included AFTER astro-auto-import
+  AutoImport({
+    imports: [
+      // Import a component’s default export
+      // generates:
+      // import A from './src/components/A.astro';
+      "./src/components/A.astro",
+
+      {
+        // Explicitly alias a default export
+        // generates:
+        // import { default as B } from './src/components/B.astro';
+        "./src/components/B.astro": [["default", "B"]],
+
+        // Import a module’s named exports
+        // generates:
+        // import { Tweet, YouTube } from 'astro-embed';
+        "astro-embed": ["Tweet", "YouTube"],
+
+        // Import all named exports from a module as a namespace
+        // generates:
+        // import * as Components from './src/components';
+        "./src/components": "Components",
+      },
+    ],
+  }), mdx()],
 });
